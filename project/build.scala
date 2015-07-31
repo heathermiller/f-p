32c92d02707facc7b2e8e5c23a8cf68c20ddf0f5
Normen Müller <normen.mueller@gmail.com>
2015-07-31 15:13:37 +0200

    Refactor f-p core
    This resolves #21.

---
+++ project/build.scala
@@ -0,0 +1,55 @@
+import sbt._
+import Keys._
+
+import com.typesafe.sbt.SbtMultiJvm
+import com.typesafe.sbt.SbtMultiJvm.MultiJvmKeys.MultiJvm
+
+object build extends Build {
+  type Sett = Def.Setting[_]
+
+  lazy val standardSettings: Seq[Sett] = Seq[Sett](
+    scalaVersion := "2.11.7",
+    scalacOptions ++= Seq(
+      "-deprecation",
+      "-encoding", "UTF-8",
+      "-feature",
+      "-unchecked"
+    ), 
+    resolvers ++= (if (version.value.endsWith("-SNAPSHOT")) List(Resolver.sonatypeRepo("snapshots")) else Nil),
+    parallelExecution in Global := false,
+    testOptions in Test += Tests.Argument(TestFrameworks.JUnit, "-q", "-v", "-s")
+  )  
+
+  lazy val `f-p` = Project(
+    id = "f-p",
+    base = file("."),
+    settings = standardSettings,
+    aggregate = Seq(core, samples)
+  ) 
+
+  lazy val core = Project(
+    id = "core",
+    base = file("core"),
+    settings = standardSettings ++ SbtMultiJvm.multiJvmSettings ++ Seq[Sett](
+      name := "f-p core",
+      libraryDependencies ++= Seq(
+        "org.scala-lang.modules" %% "spores-core"     % "0.1.3",
+        "org.scala-lang.modules" %% "spores-pickling" % "0.1.3",
+        "io.netty" % "netty-all" % "4.0.30.Final",
+        "com.typesafe.akka" % "akka-actor_2.11" % "2.3.12",
+        "junit"        % "junit-dep"       % "4.11" % "test",
+        "com.novocode" % "junit-interface" % "0.11" % "test"
+      ),
+      compile in MultiJvm <<= (compile in MultiJvm) triggeredBy (compile in Test)
+    )
+  ) configs (MultiJvm)
+
+  lazy val samples = Project(
+    id = "samples",
+    base = file("samples"),
+    settings = standardSettings ++ Seq[Sett](
+      name := "f-p samples"
+    )
+  ) dependsOn(`core`)
+
+}
