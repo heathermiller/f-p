d2a0cfd679b48f4dec1ea1d95edcced69ecd9dba
Jocelyn Boullier <jocelyn.boullier@epfl.ch>
2016-04-02 15:22:46 +0200

    Remove commented code in NodeActor

--- core/src/main/scala/actors/NodeActor.scala
+++ core/src/main/scala/actors/NodeActor.scala
@@ -57,51 +57,6 @@ class NodeActor(system: SiloSystemInternal) extends Actor {
 
   def resetPromise(id: Int): Unit = promiseOf -= id
 
-//   class RemoteEmitter[T](destNodeActor: ActorRef, emitterId: Int, destRefId: Int) extends Emitter[T] {
-//     def emit(v: T)(implicit pickler: Pickler[T], unpickler: Unpickler[T]): Unit = {
-//       // println(s"using pickler of class type ${pickler.getClass.getName} to pickle $v")
-
-//       // val p = v.pickle
-
-//       try {
-//         // 1. pickle value
-// /*
-//         val builder = pickleFormat.createBuilder()
-//         builder.hintTag(pickler.tag)
-//         pickler.pickle(v, builder)
-//         val p = builder.result()
-// */
-//         val p = (v: Any).pickle
-//         numPickled.incrementAndGet()
-
-//         // 2. create SelfDescribing instance
-//         val sd = SelfDescribing(unpickler.getClass.getName, p.value)
-
-//         // 3. pickle SelfDescribing instance
-//         val sdp = sd.pickle
-//         val ba = sdp.value
-//         destNodeActor ! Emit(emitterId, destRefId, ba)
-//       } catch {
-//         case t: Throwable =>
-//           println(s"caught $t")
-//           t.printStackTrace()
-//       }
-//     }
-//     def done(): Unit = {
-//       destNodeActor ! Done(emitterId, destRefId)
-//     }
-//   }
-
-  // def mkEmitter[T, R](emitterId: Int, destHost: Host, destRefId: Int, builder: AbstractBuilder { type Elem = T; type Coll = R }): Future[Emitter[T]] = {
-  //   val destNodeActor = Config.m(destHost)
-
-  //   val createSiloMsg = CreatSilo(emitterId, destRefId, builder)
-
-  //   (destNodeActor ? createSiloMsg).map { x =>
-  //     new RemoteEmitter[T](destNodeActor, emitterId, destRefId)
-  //   }
-  // }
-
   def respondSilo(promise: Promise[LocalSilo[_]], sender: ActorRef, silo: LocalSilo[_], refId: Int): Unit = {
     promise.success(silo)
     println(s"responding to ${sender.path.name}")
@@ -139,7 +94,11 @@ class NodeActor(system: SiloSystemInternal) extends Actor {
             case _ => println(s"Can't do anything, type of inst is ${inst.getClass}")
           }
         } catch {
-          case e: Exception => s ! ForceError(e)
+          case e: Exception => {
+            println("EXCEPTION EXCEPTION EXCEPTION")
+            e.printStackTrace()
+            s ! ForceError(e)
+          }
         }
       }
 
@@ -151,13 +110,21 @@ class NodeActor(system: SiloSystemInternal) extends Actor {
       // IDEA: same pattern for spores, again!
       val s = sender
       Future {
-        val newSilo = fun(())
-        promise.success(newSilo)
+        try {
+          val newSilo = fun(())
+          promise.success(newSilo)
 
-        println(s"SERVER: created $newSilo. responding...")
-        val replyMsg = OKCreated(refId)
-        replyMsg.id = theMsg.id
-        s ! replyMsg
+          println(s"SERVER: created $newSilo. responding...")
+          val replyMsg = OKCreated(refId)
+          replyMsg.id = theMsg.id
+          s ! replyMsg
+        } catch {
+          case e: Exception => {
+            println("EXCEPTION EXCEPTION EXCEPTION")
+            e.printStackTrace()
+            s ! ForceError(e)
+          }
+        }
       }
 
     case msg @ ForceMessage(idVal) =>
@@ -208,7 +175,8 @@ class NodeActor(system: SiloSystemInternal) extends Actor {
                 }
               } catch {
                 case e: Exception => {
-                  // println(s"Error during execution.. ${e.getMessage}")
+                  println("EXCEPTION EXCEPTION EXCEPTION")
+                  e.printStackTrace()
                   localSender ! ForceError(e)
                 }
               }
@@ -237,6 +205,8 @@ class NodeActor(system: SiloSystemInternal) extends Actor {
               }
             } catch {
               case e: Exception => {
+                println("EXCEPTION EXCEPTION EXCEPTION")
+                e.printStackTrace()
                 localSender ! ForceError(e)
               }
             }
@@ -248,134 +218,7 @@ class NodeActor(system: SiloSystemInternal) extends Actor {
             s ! ForceResponse(silo.value)
           }
 
-        // case m: MultiInput[r] =>
-        //   val inputs    = m.inputs
-        //   val refId     = m.refId
-        //   val emitterId = m.emitterId
-
-        //   val s = sender
-        //   val promise = getOrElseInitPromise(refId)
-
-        //   // prepare builder
-        //   inputs match {
-        //     case List()  =>
-        //       promise.failure(new NoSuchElementException("no input silo"))
-        //     case x :: xs =>
-        //       println(s"NODE $refId: creating builder")
-        //       val builder = x.bf.mkBuilder()
-        //       // add mapping for emitterId
-        //       // builder, num completed, required completed
-        //       val triple = (builder, 0, inputs.size)
-        //       builderOfEmitterId += (emitterId -> triple)
-        //       // send DoPumpTo messages to inputs
-        //       inputs.foreach { case input: PumpNodeInput[u, v, r, p] =>
-        //         val host = system.location(input.from.refId)
-        //         val inputNodeActor = Config.m(host)
-        //         // must also send node (input.from), so that the input silo can be completed first
-        //         println(s"NODE $refId: sending DoPumpTo to ${input.from.refId}")
-        //         inputNodeActor ! DoPumpTo[u, v, p](input.from, input.fun.asInstanceOf[p], input.pickler, input.unpickler, emitterId, system.location(refId), refId)
-        //       }
-        //       // register completion for responding with ForceResponse
-        //       promise.future.foreach { (silo: LocalSilo[_]) =>
-        //         s ! ForceResponse(silo.value)
-        //       }
-        //   }
-        //   resetPromise(refId)
       }
 
-    // case class DoPumpTo[A, B](node: Node, fun: (A, Emitter[B]) => Unit, emitterId: Int, destHost: Host, destRefId: Int)
-    // case pump: DoPumpTo[a, b, p] =>
-    //   val node      = pump.node
-    //   val fun       = pump.fun
-    //   val emitterId = pump.emitterId
-    //   val destHost  = pump.destHost // currently unused, but might be useful for Netty backend
-    //   val destRefId = pump.destRefId
-
-    //   println(s"NODE ${node.refId}: received DoPumpTo")
-
-    //   val emitter = new RemoteEmitter[b](sender, emitterId, destRefId)
-
-    //   print(s"NODE ${node.refId}: getOrElseInitPromise... ")
-    //   val promise = getOrElseInitPromise(node.refId)
-    //   promise.future.foreach { localSilo =>
-    //     // println(s"SERVER: calling doPumpTo on local Silo (${localSilo.value})")
-    //     localSilo.doPumpTo[a, b](fun.asInstanceOf[(a, Emitter[b]) => Unit], emitter)
-    //   }
-
-    //   // kick off materialization
-    //   // println(s"NODE ${node.refId}: kick off materialization by sending Graph($node)")
-    //   self ! Graph(node, false)
-
-    /*
-     *  @tparam a old element type
-     *  @tparam b new element type
-     *  @tparam c new collection type
-     */
-    // case pumpTo: PumpTo[a, b, c] =>
-    //   val emitterId   = pumpTo.emitterId
-    //   val srcRefId    = pumpTo.srcRefId
-    //   val destHost    = pumpTo.destHost
-    //   val destRefId   = pumpTo.destRefId
-    //   val fun         = pumpTo.fun
-    //   val bf          = pumpTo.bf
-    //   println(s"SERVER: initiating pumpTo from $srcRefId to $destRefId @ $destHost...")
-
-    //   // at this point, we know:
-    //   //   fun: (a, Emitter[b]) => Unit
-    //   //   bf: BuilderFactory[a, b]
-
-    //   val fut: Future[Emitter[b]] =
-    //     mkEmitter[b, c](emitterId, destHost, destRefId, bf.mkBuilder())
-
-    //   val localSiloFut = promiseOf(srcRefId).future
-
-    //   fut.zip(localSiloFut).foreach { case (emitter, localSilo) =>
-    //     // println(s"SERVER: calling doPumpTo on local Silo (${localSilo.value})")
-    //     localSilo.doPumpTo[a, b](fun, emitter)
-    //   }
-
-    // case CreatSilo(emitterId, destRefId, builder) =>
-    //   println(s"node actor ${self.path.name} received CreateSilo msg")
-    //   // builderOfEmitterId += (emitterId -> builder)
-    //   val triple = (builder, 0, 1)
-    //   builderOfEmitterId += (emitterId -> triple)
-    //   sender ! "Done"
-
-    // case Emit(emitterId, destRefId, ba) =>
-    //   // println(s"node actor ${self.path.name} received Emit msg")
-    //   builderOfEmitterId.get(emitterId) match {
-    //     case None => ???
-    //     case Some((builder, current, required)) =>
-    //       val pickle = BinaryPickleArray(ba.asInstanceOf[Array[Byte]])
-    //         // JSONPickle(ba.asInstanceOf[String])
-    //       val sdv = pickle.unpickle[SelfDescribing] // *static* unpickling
-    //       // val v = sdv.result()
-    //       val pickle0 = BinaryPickleArray(sdv.blob)
-    //       val v = pickle0.unpickle[Any] // runtime unpickling
-    //       // println(s"received ${v.toString}")
-    //       val stableBuilder = builder
-    //       stableBuilder += v.asInstanceOf[stableBuilder.Elem]
-    //   }
-
-  //   case Done(emitterId, destRefId) =>
-  //     builderOfEmitterId.get(emitterId) match {
-  //       case None => ???
-  //       case Some((builder, current, required)) =>
-  //         if (current == required - 1) {
-  //           // we're done, last emitter finished
-
-  //           // here we need to call result() on the builder
-  //           // but it would be better to directly create a local Silo
-  //           val newSilo = builder.resultSilo()
-
-  //           // complete promise for destRefId, so that force calls can complete
-  //           promiseOf(destRefId).success(newSilo)
-
-  //           // println(s"SERVER: created silo containing: ${newSilo.value}")
-  //         } else {
-  //           val newTriple = (builder, current + 1, required)
-  //           builderOfEmitterId += (emitterId -> newTriple)
-  //         }
-  //     }
   }
 }
